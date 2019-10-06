d6ff520586c7344b72c6f8c8ea14720202f854c4
gflan <glancioni@dc.uba.ar>
2019-10-06 16:12:33 -0300

    Tests para deflación y método de la potencia

--- src/test/test.py
+++ src/test/test.py
@@ -1,16 +1,20 @@
 # coding=utf-8
 """
+Tests de eigen, knn y pca
+correr como
+python3 test.py
 """
 # Estas dos líneas permiten que python encuentre la librería sentiment en notebooks/
 import sys
-sys.path.append("notebooks/")
+sys.path.append("../../notebooks/")
 
 import pandas as pd
 import numpy as np
 import math
 import traceback
 
-from sentiment import PCA, KNNClassifier, power_iteration, get_first_eigenvalues
+# PCA, KNNClassifier, power_iteration, get_first_eigenvalues
+import sentiment as st
 
 #copiado de los tests del taller1
 def mntest(func):
@@ -52,10 +56,110 @@ def testear_pm_diagonal_3x3():
     A = np.diag([1.,2.,3.])
     A_reversed = np.diag([1.,2.,3.][::-1])
 
-    np.linalg.eig(A)
+    c1, b1 =  st.power_iteration(A, num_iter=15000)
+    c2, b2 =  st.power_iteration(A_reversed, num_iter=15000)
 
-    #assert()
+    # norma 1
+    assert(abs(np.linalg.norm(b1)-1) < 1e-4)
+    assert(abs(np.linalg.norm(b2)-1) < 1e-4)
+    # es autovector y autovalor
+    assert(np.allclose(A@b1, c1*b1, rtol = 1e-4 , atol = 1e-6))
+    assert(np.allclose(A_reversed@b2, c2*b2, rtol = 1e-4 , atol = 1e-6))
+    # es el autovalor más grande
+    assert(abs(c1 - 3.) < 1e-15)
+    assert(abs(c2 - 3.) < 1e-15)
+
+@mntest
+def testear_pm_diagonal_4x4():
+    A = np.diag([1.,2.,3.,4.])
+    A_reversed = np.diag([1.,2.,3.,4.][::-1])
+
+    c1, b1 =  st.power_iteration(A, num_iter=15000)
+    c2, b2 =  st.power_iteration(A_reversed, num_iter=15000)
+
+    # norma 1
+    assert(abs(np.linalg.norm(b1)-1) < 1e-4)
+    assert(abs(np.linalg.norm(b2)-1) < 1e-4)
+    # es autovector y autovalor
+    assert(np.allclose(A@b1, c1*b1, rtol = 1e-4 , atol = 1e-6))
+    assert(np.allclose(A_reversed@b2, c2*b2, rtol = 1e-4 , atol = 1e-6))
+    # es el autovalor más grande
+    assert(abs(c1 - 4.) < 1e-15)
+    assert(abs(c2 - 4.) < 1e-15)
+
+@mntest
+def testear_pm_diagonalizable_5x5():
+    d = np.diag([1 ,2 ,3 ,4 ,5])
+    v = np.array([
+        [1,0,0,0,0],
+        [2,2,3,0,0],
+        [3,0,2,3,1],
+        [4,4,2,0,0],
+        [5,4,2,0,1]
+    ])
+
+    A = v @ d @ v.T
+    eigenvals, _ = np.linalg.eig(A)
+    c1, b1 =  st.power_iteration(A, num_iter=15000)
+
+    # norma 1
+    assert(abs(np.linalg.norm(b1)-1) < 1e-15)
+    # es autovector y autovalor
+    assert(np.allclose(A@b1, c1*b1))
+    # es el autovalor más grande
+    assert(abs(c1 - eigenvals[0]) < 1e-13)
 
 
+"""
+Deflación
+"""
+
+@mntest
+def testear_deflacion_diagonalizable_5x5():
+    d = np.diag([1 ,2 ,3 ,4 ,5])
+    v = np.array([
+        [1,0,0,0,0],
+        [2,2,3,0,0],
+        [3,0,2,3,1],
+        [4,4,2,0,0],
+        [5,4,2,0,1]
+    ])
+
+    A = v @ d @ v.T
+    eigenvals, _ = np.linalg.eig(A)
+    c, b =  st.get_first_eigenvalues(A, 5, num_iter=15000)
+
+    # norma 1
+    for i in range(5):
+        assert(abs(np.linalg.norm(b[:,i])-1) < 1e-15)
+    # es autovector y autovalor
+    for i in range(5):
+        assert(np.allclose(A@b[:,i], c[i]*b[:,i]))
+    # es el autovalor más grande
+    assert(np.allclose(sorted(eigenvals, reverse=True), c))
+
+@mntest
+def testear_deflacion_diagonalizable_6x6():
+    d = np.diag([1 ,2 ,3 ,4 ,5, 6])
+    v = np.array([
+        [1,0,0,0,0,0],
+        [2,2,3,0,0,0],
+        [3,0,2,3,1,0],
+        [4,4,2,0,0,0],
+        [3,1,2,0,0,1],
+        [5,4,2,0,1,0]
+    ])
+    A = v @ d @ v.T
+    eigenvals, _ = np.linalg.eig(A)
+    c, b =  st.get_first_eigenvalues(A, 6, num_iter=15000)
+
+    # norma 1
+    for i in range(6):
+        assert(abs(np.linalg.norm(b[:,i])-1) < 1e-15)
+    # es el autovalor más grande
+    assert(np.allclose(sorted(eigenvals, reverse=True), c))
+    # es autovector y autovalor
+    for i in range(6):
+        assert(np.allclose(A@b[:,i], c[i]*b[:,i]))
 
 correr_tests()
