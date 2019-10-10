6eba2875a0c11ba96a80dfba3badc4877d250956
cato <cdelia@dc.uba.ar>
2019-10-10 16:06:07 -0300

    como parametro si es skelearn o sentiment

--- bin/run-multiple-climbers-big.sh
+++ bin/run-multiple-climbers-big.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-time for a in {500..1100..100}; do for k in {100..1100..100}; do ./searcher.py --algorithm hill-climbing -k $k --alpha $a --k-step 10 --alpha-step 10 --like-classify sklearn; done; done
+time for a in {500..1100..100}; do for k in {100..1100..100}; do ./searcher.py --algorithm hill-climbing -k $k --alpha $a --k-step 10 --alpha-step 10 --like-classify $1; done; done
