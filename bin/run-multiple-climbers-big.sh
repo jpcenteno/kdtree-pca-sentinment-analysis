2f5618e0c3b9f96410bd7743e5c0eda011c8bb8f
cato <cdelia@dc.uba.ar>
2019-10-26 03:43:22 -0300

    aprovecho alfa más grande para la memoización de pca y otra yerbas
    * logger: _f file para hacer flush on write
    * cambio script para que se encarge todo searcher así aprovecha todo
      lo posible la memoización

--- bin/run-multiple-climbers-big.sh
+++ bin/run-multiple-climbers-big.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-time for a in {500..1100..100}; do for k in {100..1100..100}; do ./searcher.py --algorithm hill-climbing -k $k --alpha $a --k-step 10 --alpha-step 10 --like-classify $1; done; done
+./searcher.py --algorithm hill-climbing --like-classify \
+              --k-step 10 --alpha-step 10 \
+              --grid-k {100..1000..100} --grid-alpha {1100..500..100} \
+              -imp $1
